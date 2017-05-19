f23b14e91f9b0f161084af908409bbf6d906087e
Joel Fernandes <joelaf@google.com>
2017-05-18 22:46:10 -0700

    FROMLIST: sched: Make iowait_boost optional in schedutil
    We should apply the iowait boost only if cpufreq policy has iowait boost
    enabled. Also make it a schedutil configuration from sysfs so it can be
    turned on/off if needed (by default initialize it to the policy value).
    
    For systems that don't need/want it enabled, such as those on arm64
    based mobile devices that are battery operated, it saves energy when the
    cpufreq driver policy doesn't have it enabled (details below):
    
    Here are some results for energy measurements collected running a
    YouTube video for 30 seconds:
    Before: 8.042533 mWh
    After: 7.948377 mWh
    Energy savings is ~1.2%
    
    Bug: 38010527
    Link: https://lkml.org/lkml/2017/5/19/42
    Change-Id: If124076ad0c16ade369253840dedfbf870aff927
    Signed-off-by: Joel Fernandes <joelaf@google.com>
    [yaro]: set to false by default.
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -27,6 +27,7 @@ struct sugov_tunables {
 	unsigned int		down_rate_limit_us;
 	unsigned int hispeed_load;
 	unsigned int hispeed_freq;
+	bool iowait_boost_enable;
 	bool pl;
 };
 
@@ -314,6 +315,11 @@ static void sugov_get_util(unsigned long *util, unsigned long *max, int cpu)
 static void sugov_set_iowait_boost(struct sugov_cpu *sg_cpu, u64 time,
 				   unsigned int flags)
 {
+	struct sugov_policy *sg_policy = sg_cpu->sg_policy;
+
+	if (!sg_policy->tunables->iowait_boost_enable)
+		return;
+
 	if (flags & SCHED_CPUFREQ_IOWAIT) {
 		if (sg_cpu->iowait_boost_pending)
 			return;
@@ -739,6 +745,28 @@ static ssize_t hispeed_freq_store(struct gov_attr_set *attr_set,
 		sg_policy->hispeed_util = hs_util;
 		raw_spin_unlock_irqrestore(&sg_policy->update_lock, flags);
 	}
+	
+	return count;
+}
+
+static ssize_t iowait_boost_enable_show(struct gov_attr_set *attr_set,
+					char *buf)
+{
+	struct sugov_tunables *tunables = to_sugov_tunables(attr_set);
+
+	return sprintf(buf, "%u\n", tunables->iowait_boost_enable);
+}
+
+static ssize_t iowait_boost_enable_store(struct gov_attr_set *attr_set,
+					 const char *buf, size_t count)
+{
+	struct sugov_tunables *tunables = to_sugov_tunables(attr_set);
+	bool enable;
+
+	if (kstrtobool(buf, &enable))
+		return -EINVAL;
+
+	tunables->iowait_boost_enable = enable;
 
 	return count;
 }
@@ -765,6 +793,7 @@ static struct governor_attr up_rate_limit_us = __ATTR_RW(up_rate_limit_us);
 static struct governor_attr down_rate_limit_us = __ATTR_RW(down_rate_limit_us);
 static struct governor_attr hispeed_load = __ATTR_RW(hispeed_load);
 static struct governor_attr hispeed_freq = __ATTR_RW(hispeed_freq);
+static struct governor_attr iowait_boost_enable = __ATTR_RW(iowait_boost_enable);
 static struct governor_attr pl = __ATTR_RW(pl);
 
 static struct attribute *sugov_attributes[] = {
@@ -772,6 +801,7 @@ static struct attribute *sugov_attributes[] = {
 	&down_rate_limit_us.attr,
 	&hispeed_load.attr,
 	&hispeed_freq.attr,
+	&iowait_boost_enable.attr,
 	&pl.attr,
 	NULL
 };
@@ -968,6 +998,7 @@ static int sugov_init(struct cpufreq_policy *policy)
 	tunables->hispeed_load = DEFAULT_HISPEED_LOAD;
 	tunables->hispeed_freq = 0;
 
+	tunables->iowait_boost_enable = false;
 	policy->governor_data = sg_policy;
 	sg_policy->tunables = tunables;
 	stale_ns = sched_ravg_window + (sched_ravg_window >> 3);
